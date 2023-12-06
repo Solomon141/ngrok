0affac4931899e4a824a5c17e6282dc9d14a5ad1
solomon141 <weymit2001@gmail.com>
2023-12-06 07:51:14 +0300

    sol

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const PORT = process.env.PORT || 4040;
+
+const app = express();
+app.use(express.json());
+
+app.post("*", async (req, res) => {
+    res.send("Hello post!");
+});
+
+
+app.get("*", async (req, res) => {
+    res.send("Hello Get!");
+});
+
+
+app.listen(PORT, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log("server listening on port", PORT);
+})
